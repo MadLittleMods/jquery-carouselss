bee97d7f5ce64d8273e9207fdf16cebe4bcbb69c
Eric Eastwood <contact@ericeastwood.com>
2015-01-16 13:16:46 -0600

    Fix AMD part of the UMD pattern

--- jquery-carouselss.js
+++ jquery-carouselss.js
@@ -7,9 +7,9 @@
 (function (root, factory) {
 	if (typeof define === 'function' && define.amd) {
 		// AMD. Register as an anonymous module.
-		define(['jquery'], (function($) {
+		define(['jquery'], function($) {
 			return factory($);
-		})($));
+		});
 	} else if (typeof exports === 'object') {
 		// Node. Does not work with strict CommonJS, but
 		// only CommonJS-like environments that support module.exports,
@@ -618,6 +618,8 @@
 
 
 
+
 	return Carousel;
+	
 
 }));
